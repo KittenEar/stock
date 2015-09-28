f2cf18a6da83f1d3d143d0e658f0d251e25bdfed
KittenEar <CarryCat@cat-07-no-MacBook-Pro.local>
2015-09-29 06:05:48 +0900

    ログ出力ファイル名変更

--- Log.php
+++ Log.php
@@ -6,7 +6,7 @@ ini_set('log_errors', 1);
 class Log
 {
     public static function error($message) {
-        error_log($message . PHP_EOL, 3, "./php_error.log");
+        error_log($message . PHP_EOL, 3, "./php.log");
     }
 }
 
