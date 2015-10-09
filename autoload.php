dfdbcaa1c2d49625c7a08839e4c10861aab8834d
KittenEar <CarryCat@cat-07-no-MacBook-Pro.local>
2015-10-09 13:00:49 +0900

    マニュアルでは __autoload() を非推奨としているため使用しない

--- autoload.php
+++ autoload.php
@@ -3,12 +3,18 @@
 /**
  * クラスのオートローディング
  */
-function __autoload($name)
-{
-    $file = $name . '.php';
+// function __autoload($name)
+// {
+//     $file = $name . '.php';
+//
+//     if (is_readable($file)) {
+//         require_once $file;
+//     }
+// }
 
-    if (is_readable($file)) {
-        require_once $file;
-    }
+function stockAutoloader($name) {
+    include $name . '.php';
 }
+
+spl_autoload_register('stockAutoloader');
 
